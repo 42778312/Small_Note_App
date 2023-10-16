bf40c086754faddd761a20fb5d2295334c3d8705
Mohammed Al-Tabaei <117546290+42778312@users.noreply.github.com>
2023-10-16 15:10:41 +0200

    no edit

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -11,7 +11,7 @@ namespace SimpleCalendarApp
         static Dictionary<DateTime, string> calendar = new Dictionary<DateTime, string>();
 
         static void Main(string[] args)
-            // test 
+            
         {
             while (true)
             {
